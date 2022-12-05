f0f9a7b1222d8e31ded9208ce5aabf13965c0e37
mkrechetov <42608119+mkrechetov@users.noreply.github.com>
2022-12-05 18:51:17 +0300

    Add files via upload

--- cpp/137_SingleNumberII.cpp
+++ cpp/137_SingleNumberII.cpp
@@ -7,53 +7,87 @@
 
 using namespace std;
 
-const int trits = 30;
+struct trit {
+  bool a = false;
+  bool b = false;
 
-list<int> ternary(int num) {
-  list<int> t;
-  if (num < 0) { t.push_back(1);} else { t.push_back(2); }
-  for (int i=1; i<trits; i++) { t.push_back(abs(num % 3)); num /= 3; }
-  return t;
+  inline trit operator=(trit t) { a=t.a; b=t.b; return t; }
+  inline bool operator==(trit t) {
+    if (t.a==a && t.b== b) return true;
+    else return false;
+  }
+};
+
+const trit trit0 = trit{false, false};
+const trit trit1 = trit{false, true};
+const trit trit2 = trit{true, false};
+const trit tritF = trit{true, true};
+
+trit operator+(trit t1, trit t2) {
+  trit t = trit{};
+  t.a = t1.a^t2.a^(t1.b && t2.b);
+  t.b = t1.b^t2.b^(t1.a && t2.a);
+  if (t == tritF) { return trit0; }
+  else { return t; }
 }
 
-void print(list<int> a) {
-  for (auto const& trit : a) { cout << trit; }
+ostream& operator << (ostream& o, const trit t) {
+    if ((t.a == false) && (t.b == false)) o << 0;
+    else if ((t.a == false) && (t.b == true)) o << 1;
+    else if ((t.a == true) && (t.b == false)) o << 2;
+    else o << "*";
+    return o;
+}
+
+const int ntrits = 30;
+
+trit* ternary(int num) {
+  static trit trits[ntrits];
+  if (num < 0) { trits[0] = trit1;}
+  else { trits[0] = trit2; }
+
+  int r;
+  for (int i=1; i<ntrits; i++) {
+    r = abs(num % 3);
+    if (r == 0) { trits[i] = trit0; }
+    else if (r == 1) { trits[i] = trit1; }
+    else { trits[i] = trit2; }
+    num = num / 3;
+  }
+  return trits;
+}
+
+void print(trit* trits) {
+  for (int i=0; i<ntrits; i++) { cout << trits[i]; }
   cout << endl;
 }
 
-list<int> terxor(list<int> a, list<int> b) {
-  list<int> t;
-  list<int>::iterator it_a = a.begin();
-  list<int>::iterator it_b = b.begin();
-
-  for (int i=0; i<trits; i++) {
-    t.push_back(((*it_a) + (*it_b)) % 3);
-    ++it_a;
-    ++it_b;
-  }
-  return t;
+void tritxor(trit* a, int num) {
+  trit* b = ternary(num);
+  for (int i=0; i<ntrits; i++) { a[i] = a[i] + b[i]; }
 }
 
-int from_ternary(list<int> t) {
+int from_trits(trit* t) {
   int result = 0;
   int sign = 1;
-  list<int>::iterator it = t.begin(); ++it;
 
-  if (t.front() == 1) { sign = -1; }
-  for (int i=1; i<trits; i++) {
-      result += sign * (*it) * pow(3, i-1);
-      ++it;
+  if (t[0] == trit1) { sign = -1; }
+  for (int i=1; i<ntrits; i++) {
+    if (t[i] == trit2) {
+      result += sign * 2 * pow(3, i-1);
+    } else if (t[i] == trit1) {
+      result += sign * 1 * pow(3, i-1);
+    }
   }
 
   return result;
 }
 
 int singleNumber(vector<int> nums) {
-  list<int> t;
-  for (int i=0; i<trits; i++) { t.push_back(0); }
-  for (int num: nums) { t = terxor(t, ternary(num)); }
-  print(t);
-  return from_ternary(t);
+  trit t[ntrits] = { trit0 };
+  for (int num: nums) { tritxor(t, num); }
+  // print(t);
+  return from_trits(t);
 }
 
 int main(int argc, char* argv[]) {
